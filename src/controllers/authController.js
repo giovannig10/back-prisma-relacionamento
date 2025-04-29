76e00fd19569f619dd6715d337d5d7434f538ee7
giovanni <giovanni.ggoncalves@outlook.com>
2025-04-29 11:02:29 -0300

    feat: create login

--- src/controllers/authController.js
+++ src/controllers/authController.js
@@ -1,5 +1,7 @@
 import userModel from "../models/userModel.js";
 import bcrypt from "bcryptjs";
+import jwt from "jsonwebtoken";
+
 
 class AuthController {
   //Listar todos os usuários
@@ -51,6 +53,50 @@ class AuthController {
     } catch (error) {
         console.error("Erro ao registrar usuário:", error);
         res.status(500).json({ error: "Erro ao registrar usuário" });
+    }
+  }
+
+  //Login do usuário
+  async login(req,res) {
+    try {
+        const { email, password} = req.body;
+
+        if (!email || !password) {
+            return res
+              .status(400)
+              .json({ error: "Os campos nome, email e senha são obrigátorios" });
+          }
+
+          //Verificar se o usuário existe
+      const userExists = await userModel.findByemail(email);
+      if (!userExists) {
+        return res.status(401).json({ error: "Credenciais invalidas!" });
+      }
+
+      //Verificar senha
+      const isPasswordValid = await bcrypt.compare(password, userExists.password);
+      if (!isPasswordValid) {
+        return res.status(401).json({ error: "Credenciais invalidas!"});
+      }
+
+        //Gerar token
+        const token = jwt.sign({id: userExists.id,
+            name: userExists.name,
+            email: userExists.email
+        },
+        process.env.JWT_SECRET, {
+            expiresIn: "24h",
+        }
+        );
+ 
+        return res.status(200).json({
+            message: "Login realizado com sucesso!",
+            token,
+            userExists,
+        });
+    } catch (error) {
+        console.error("Erro ao fazer login:", error);
+        res.status(500).json({ error: "Erro ao fazer login" });
     }
   }
 }
